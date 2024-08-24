635c21e59275641f359d35c5a114a73be037acfb
Kabeer-Ahmed-07 <kabeerbaba0300@gmail.com>
2024-08-24 17:08:00 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+/*// assignment 1 :- write a script to greet your website visitor using js alert box
+
+alert ('Hello kabeer')
+
+// assignment 2 :- write a script to display following message on your web page :
+
+alert ('Error! Please Enter a Valid Password')
+
+// assignment 3 :- write a script to display massage on youe web page :
+
+alert ('Welcome to JS land...\nHappy coding!')*/
+
+let fruits = ['apple' ,'Banana' ,'mango' , 'pear' ,'kharbuza' , 5]
+
+let firstFruit = fruits[0];
+
+fruits[5] = 'charry'
+
+console.log(firstFruit)
+console.log(fruits);
